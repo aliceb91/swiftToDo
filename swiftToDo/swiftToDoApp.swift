64b0866747ad04d415b1cf0ebb7b32af96f5fff5
Alice Birkett <alice.birkett@outlook.com>
2023-10-04 10:53:10 +0100

    Initial Commit

---
+++ swiftToDo/swiftToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftToDoApp.swift
+//  swiftToDo
+//
+//  Created by Alice Birkett on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
